5647ba0e45e38092f0fc4fecfc44066b7ada1907
xRascalx <jk00242004@gmail.com>
2022-08-31 16:21:13 +0800

    在controller中處理異常

--- src/main/java/com/lrm/web/BookController.java
+++ src/main/java/com/lrm/web/BookController.java
@@ -1,17 +1,21 @@
 package com.lrm.web;
 
 import com.lrm.domain.Book;
-import com.lrm.exception.BookNotFoundException;
 import com.lrm.service.BookService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/books")
 public class BookController {
+    private final Logger logger = LoggerFactory.getLogger(BookController.class);
 
     private BookService bookService;
 
@@ -22,10 +26,27 @@ public class BookController {
     @GetMapping("/{id}")
     public String getBook(@PathVariable Long id, Model model) {
         Book book = bookService.getBookById(id);
-        if(book == null){
-            throw new BookNotFoundException("書單訊息不存在");
-        }
         model.addAttribute("book", book);
         return "book";
+    }
+
+    /**
+     * 異常處理
+     *
+     * @param request
+     * @param e
+     * @return
+     */
+    @ExceptionHandler({Exception.class})
+    public ModelAndView handleException(HttpServletRequest request, Exception e) throws Exception {
+        logger.error("Request URL : {}, Exception : {}", request.getRequestURI(), e.getMessage());
+        if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
+            throw e;
+        }
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("url", request.getRequestURI());
+        mav.addObject("exception", e);
+        mav.setViewName("error/error");
+        return mav;
     }
 }
